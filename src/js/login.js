2b19bfa26c07bfc975cf6183418c608929598cd8
haplr <ralphleonard78@gmail.com>
2026-04-10 22:59:04 -0400

    Updated login page and js

--- src/js/login.js
+++ src/js/login.js
@@ -1,41 +1,39 @@
-let debounce;
+class App {
+    // Setup function for the login page.
+    constructor() {
+        const button = document.querySelector(".login-button");
+        const username = document.querySelector("#username");
+        const password = document.querySelector("#password");
+        this.errorDiv = document.querySelector("#error-message");
+        this.debounce;
 
-// Fetch and return users account data from account.json file
-async function getUsers() {
-    const response = await fetch("data/account.json");
-    const users = await response.json();
-    return users;
-}
+        document.querySelector("#login").addEventListener("submit", e=>this.login(e, username.value, password.value));
+    }
 
-// Checks if the user and password pairing exists in the account.json file
-// if so the user is brought to the home page
-// otherwise they are briefly shown a error for 3 seconds.
-async function login(inputUsername, inputPassword, errorDiv) {
-    const users = await getUsers();
+    // Checks if the user and password pairing exists in the account.json file
+    // if so the user is brought to the home page
+    // otherwise they are briefly shown a error for 3 seconds.
+    async login(e, inputUsername, inputPassword) {
+        e.preventDefault();
+        const users = await this.getUsers();
     
-    if(users) {
-        if(users.find(user => user.username === inputUsername && user.password === inputPassword)) {
-            window.location.href = 'index.html';
-        } else {
-            clearTimeout(debounce);
-            errorDiv.classList.remove("hidden");
-            setTimeout(() => errorDiv.classList.add("hidden"), 3000);
+        if(users) {
+            if(users.find(user => user.username === inputUsername && user.password === inputPassword)) {
+                window.location.href = 'index.html';
+            } else {
+                clearTimeout(this.debounce);
+                this.errorDiv.classList.remove("hidden");
+                setTimeout(() => this.errorDiv.classList.add("hidden"), 3000);
+            }
         }
+    }
+
+    // Fetch and return users account data from account.json file
+    async getUsers() {
+        const response = await fetch("data/account.json");
+        const users = await response.json();
+        return users;
     }
 }
 
-// Setup function for the login page.
-const setup = () => {
-    const button = document.querySelector("#login");
-    const username = document.querySelector("#username");
-    const password = document.querySelector("#password");
-    const errorDiv = document.querySelector("#error-message");
-
-    document.querySelectorAll(".no-submit").forEach(t => t.addEventListener("submit", e=>{
-        login(username.value, password.value, errorDiv);
-        e.preventDefault();
-    }));
-    button.addEventListener("click", ()=>login(username.value, password.value, errorDiv));
-}
-
-export default setup;
+export default App;
